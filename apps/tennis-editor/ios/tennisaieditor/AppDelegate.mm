5a23a1323e0f20c5836b5ccb74b5f9e3d913b6c5
vaibhavverma9 <vaibhavverma9@uchicago.edu>
2025-06-18 15:23:00 -0400

    adding star

--- apps/tennis-editor/ios/tennisaieditor/AppDelegate.mm
+++ apps/tennis-editor/ios/tennisaieditor/AppDelegate.mm
@@ -60,7 +60,7 @@
   return [super application:application didReceiveRemoteNotification:userInfo fetchCompletionHandler:completionHandler];
 }
 
-- (void)application:(UIApplication )application handleEventsForBackgroundURLSession:(NSString )identifier completionHandler:(void (^)(void))completionHandler {
+- (void)application:(UIApplication )application handleEventsForBackgroundURLSession:(NSString *)identifier completionHandler:(void (^)(void))completionHandler {
 RNTusClient *tusClient = [self.bridge moduleForClass:[RNTusClient class]];
 [tusClient registerBackgroundHandler:completionHandler];
 }
